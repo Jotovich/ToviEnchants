5e7888ee4fd06446c21f899726a116002aed3d70
Jotovich <jomatovich@gmail.com>
2023-02-20 14:31:52 -0800

    Adding Tovi Enchants

---
+++ src/main/java/tovi/tovienchantments/ToviEnchantmentsClient.java
@@ -0,0 +1,9 @@
+package tovi.tovienchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToviEnchantmentsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
